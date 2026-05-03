29dd7eff4d6e3755f7787a63659b91f462b1c23d
René Theis <rene.tube992@gmail.com>
2026-05-03 14:42:43 +0200

    improvement the delete signal to delete a video file

--- video_app/signals.py
+++ video_app/signals.py
@@ -1,13 +1,22 @@
-from .models import Video
-from django.db.models.signals import post_save, post_delete
+from django.db import transaction
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
+from .models import Video
+
+
 @receiver(post_save, sender=Video)
-def video_post_save(sender, instance, created, **kwargs):
+def video_post_save(sender, instance: Video, created: bool, **kwargs) -> None:
     if created:
         print(f"New video created: {instance.title}")
+        return
     print(f"Video saved: {instance.title}")
 
+
 @receiver(post_delete, sender=Video)
-def video_post_delete(sender, instance, **kwargs):
+def video_post_delete(sender, instance: Video, **kwargs) -> None:
+    def delete_files() -> None:
+        instance.video_file.delete(save=False)
+        instance.thumbnail.delete(save=False)
+    transaction.on_commit(delete_files)
     print(f"Video deleted: {instance.title}")
